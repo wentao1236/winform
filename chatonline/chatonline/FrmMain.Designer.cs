1fc0791bb848b40747e2ee5cd587ec9a9ffd3ca7
hytcwentao <453167555@qq.com>
2014-08-25 16:46:53 +0800

    chatonline
    1.0

---
+++ chatonline/chatonline/FrmMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace chatonline
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tXtIP = new System.Windows.Forms.TextBox();
+            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tXtIP
+            // 
+            this.tXtIP.Location = new System.Drawing.Point(12, 12);
+            this.tXtIP.Name = "tXtIP";
+            this.tXtIP.Size = new System.Drawing.Size(368, 21);
+            this.tXtIP.TabIndex = 0;
+            this.tXtIP.Text = "255.255.255.255";
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Location = new System.Drawing.Point(12, 53);
+            this.txtHistory.Multiline = true;
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.ReadOnly = true;
+            this.txtHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHistory.Size = new System.Drawing.Size(363, 207);
+            this.txtHistory.TabIndex = 1;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 266);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(368, 21);
+            this.txtMsg.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(305, 304);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 334);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.tXtIP);
+            this.Name = "FrmMain";
+            this.Text = "聊天...";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tXtIP;
+        private System.Windows.Forms.TextBox txtHistory;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ chatonline/chatonline/FrmMain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+
+namespace chatonline
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string ip = this.tXtIP.Text;
+            UdpClient uc = new UdpClient();
+            string msg = "PUBLIC|"+this.txtMsg.Text+"|周大锤";//内部为内容
+            byte[] bmsg = Encoding.Default.GetBytes(msg);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+            uc.Send(bmsg,bmsg.Length,ipep);
+        }
+
+        private void listen()
+        {
+
+            UdpClient uc = new UdpClient(9527);
+            while (true)
+            {
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Any,0);
+                byte[] bmsg = uc.Receive(ref ipep);
+                string msg = Encoding.Default.GetString(bmsg);
+                string[] s = msg.Split('|');
+                if (s[0] == "PUBLIC")
+                {
+                   this.txtHistory.Text += s[2] +"："+ "\r\n";
+                   this.txtHistory.Text += s[1] + "\r\n";
+
+                }
+                else if (s[0] == "INROOM")
+                {
+                    this.txtHistory.Text += s[1] + "登录了" + "\r\n";
+                }
+                else
+                {
+                    return;
+                }
+               
+
+            }
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            string ip = this.tXtIP.Text;
+            UdpClient uc = new UdpClient();
+            string msg = "INROOM|周大锤";//发送给别人的内容
+            byte[] bmsg = Encoding.Default.GetBytes(msg);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+            uc.Send(bmsg, bmsg.Length, ipep);
+            Thread th = new Thread(new ThreadStart(listen));
+            th.Start();
+        }
+    }
+}
